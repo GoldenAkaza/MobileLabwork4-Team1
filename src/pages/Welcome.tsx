30d5a4e1b2964d4c29b63bc1ac3f506e62e3058a
hhanneh <hanne.hurskainen@gmail.com>
2025-11-30 15:59:53 +0200

    css muutoksia

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,6 +1,13 @@
 import React from 'react';
-import { IonPage, IonContent, IonButton } from '@ionic/react';
+import { IonPage, IonContent, IonButton,IonHeader, IonToolbar, IonButtons, IonTitle, IonFooter, IonTabBar, IonTabButton, IonIcon, IonLabel } from '@ionic/react';
 import { useHistory } from 'react-router-dom';
+import {   
+  homeOutline, 
+  calendarOutline, 
+  personOutline, 
+  settingsOutline
+} from 'ionicons/icons';
+import './Tasks.css';
 import './Welcome.css';
 
 const Welcome: React.FC = () => {
@@ -12,6 +19,18 @@ const Welcome: React.FC = () => {
 
   return (
     <IonPage>
+
+        <IonHeader>
+            <IonToolbar className= "custom-header">
+                <IonButtons slot="start">
+                    <IonButton fill="clear">
+                    </IonButton>
+                  </IonButtons>
+                <IonTitle>My Study Planner</IonTitle>
+            </IonToolbar>
+        </IonHeader>
+
+
       <IonContent fullscreen className="welcome-container">
         <div className="welcome-content">
           <h3>My study planner</h3>
@@ -26,6 +45,28 @@ const Welcome: React.FC = () => {
           </IonButton>
         </div>
       </IonContent>
+
+      <IonFooter>
+            <IonTabBar slot="bottom">
+                <IonTabButton tab="home" href="/home">
+                  <IonIcon icon={homeOutline} />
+                  <IonLabel>Home</IonLabel>
+                </IonTabButton>
+                <IonTabButton tab="calendar" href="/calendar">
+                  <IonIcon icon={calendarOutline} />
+                  <IonLabel>Calendar</IonLabel>
+                </IonTabButton>
+                <IonTabButton tab="profile" href="/profile">
+                  <IonIcon icon={personOutline} />
+                  <IonLabel>Profile</IonLabel>
+                </IonTabButton>
+                <IonTabButton tab="settings" href="/settings">
+                  <IonIcon icon={settingsOutline} />
+                  <IonLabel>Settings</IonLabel>
+                </IonTabButton>
+            </IonTabBar>
+        </IonFooter>
+
     </IonPage>
   );
 };
